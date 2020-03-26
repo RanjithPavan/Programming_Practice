bdba4baf466898ea9c0326b0c608da25ed4c2d4d
RanjithPavan <61808123+RanjithPavan@users.noreply.github.com>
2020-03-25 20:37:58 -0600

    Add files via upload
    This file will generate prime numbers up to a given integer.

---
+++ PrimeGenerator.py
@@ -0,0 +1,14 @@
+n = 100
+primes = [1 for i in range(n + 1)]
+primes[0], primes[1] = 0, 0
+
+current = 2
+while (current * current <= n):
+    if primes[current] == 1:
+        for i in range(current * 2, n + 1, current):
+            primes[i] = 0
+    current += 1
+
+for i in range(2, n):
+    if (primes[i] == 1):
+        print(i)
